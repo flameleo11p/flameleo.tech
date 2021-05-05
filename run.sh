a5369a26791055bf4f10f61c1545941f8eeeab34
flameleo11p <flameleo11p@protonmail.com>
2021-05-05 21:53:33 +0800

    init

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+# /drive_d/work/a2public_html/run.sh
+sudo cp -r flameleo11p.github.io/ /var/www/flameleo.tech/public_html
+sudo chown -R www-data: /var/www/flameleo.tech/public_html
+
+# https://flameleo11p.github.io/
+# flameleo.tech
