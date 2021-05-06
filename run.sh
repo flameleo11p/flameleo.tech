688ed99a9d7646b16525781c05d87eb63504307b
flameleo11p <flameleo11p@protonmail.com>
2021-05-06 10:10:30 +0800

    fix favicon.ico & images src

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ sudo cp -r public_html/ /var/www/flameleo.tech/public_html
 sudo chown -R www-data: /var/www/flameleo.tech/public_html
 ll /var/www/flameleo.tech/public_html
 
+
+# flameleo.tech
 # /drive_d/work/a2public_html/run.sh
 
 # https://flameleo11p.github.io/
