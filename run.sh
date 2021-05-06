a312b5f08620fc8dc980211596ed4100c01625d5
flameleo11p <flameleo11p@protonmail.com>
2021-05-06 10:31:06 +0800

    fix sh & todo color scheme red & blue

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 sudo rm -rf /var/www/flameleo.tech/public_html 
 sudo cp -r public_html/ /var/www/flameleo.tech/public_html
 sudo chown -R www-data: /var/www/flameleo.tech/public_html
-ll /var/www/flameleo.tech/public_html
+ls -l /var/www/flameleo.tech/public_html
 
 
 # flameleo.tech
