c62ffbf05830d270bbdb06bffea90998746b4c00
flameleo11p <flameleo11p@protonmail.com>
2021-06-03 20:08:45 +0800

    fix run.sh & add icp
    fix run.sh with current folder as website server name
    add icp

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-sudo rm -rf /var/www/flameleo.tech/public_html 
-sudo cp -r public_html/ /var/www/flameleo.tech/public_html
-sudo chown -R www-data: /var/www/flameleo.tech/public_html
-ls -l /var/www/flameleo.tech/public_html
+# sudo rm -rf /var/www/flameleo.tech/public_html 
+# sudo cp -r public_html/ /var/www/flameleo.tech/public_html
+# sudo chown -R www-data: /var/www/flameleo.tech/public_html
+# ls -l /var/www/flameleo.tech/public_html
 
 
-# flameleo.tech
-# /drive_d/work/a2public_html/run.sh
+function main() {
+  local website=`basename $(realpath .)`;
 
-# https://flameleo11p.github.io/
-# flameleo.tech
+	sudo mkdir -p /var/www/$website/public_html 
+	sudo rm -rf /var/www/$website/public_html 
+	sudo cp -r public_html/ /var/www/$website/public_html
+	sudo chown -R www-data: /var/www/$website/public_html
+	ls -l /var/www/$website/public_html
+}
+
+main()
+
+
+
